0f2a8290b6cb2616860d57251768c19da0d59262
GitHub Action <action@github.com>
2023-09-30 08:06:43 +0000

    Template cleanup

--- src/main/kotlin/com/github/fanlun007/helloideaplugin/MyBundle.kt
+++ src/main/kotlin/com/github/fanlun007/helloideaplugin/MyBundle.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template
+package com.github.fanlun007.helloideaplugin
 
 import com.intellij.DynamicBundle
 import org.jetbrains.annotations.NonNls
--- src/main/kotlin/com/github/fanlun007/helloideaplugin/listeners/MyApplicationActivationListener.kt
+++ src/main/kotlin/com/github/fanlun007/helloideaplugin/listeners/MyApplicationActivationListener.kt
@@ -1,4 +1,4 @@
-package org.jetbrains.plugins.template.listeners
+package com.github.fanlun007.helloideaplugin.listeners
 
 import com.intellij.openapi.application.ApplicationActivationListener
 import com.intellij.openapi.diagnostic.thisLogger

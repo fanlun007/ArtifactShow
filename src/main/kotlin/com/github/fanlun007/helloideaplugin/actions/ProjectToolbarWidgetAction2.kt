c8b92088e10bbe09c6025a015f355e7a4e6c1c2f
andrew <13058100289@163.com>
2023-10-03 09:27:58 +0800

    update

--- src/main/kotlin/com/github/fanlun007/helloideaplugin/actions/ProjectToolbarWidgetAction2.kt
+++ src/main/kotlin/com/github/fanlun007/helloideaplugin/actions/ProjectToolbarWidgetAction2.kt
@@ -1,12 +1,11 @@
 package com.github.fanlun007.helloideaplugin.actions
 
 import com.intellij.openapi.actionSystem.*
+import com.intellij.openapi.project.modules
 import com.intellij.openapi.ui.popup.JBPopup
 import com.intellij.openapi.ui.popup.JBPopupFactory
-import com.intellij.openapi.util.Key
 import com.intellij.openapi.wm.impl.ExpandableComboAction
 import com.intellij.openapi.wm.impl.ToolbarComboWidget
-import com.intellij.ui.util.maximumWidth
 import javax.swing.JComponent
 
 class ProjectToolbarWidgetAction2 : ExpandableComboAction() {
@@ -22,7 +21,7 @@ class ProjectToolbarWidgetAction2 : ExpandableComboAction() {
         val group = ActionManager.getInstance().getAction("MainToolbarArtifactGroup") as ActionGroup
 
         return JBPopupFactory.getInstance()
-            .createActionGroupPopup(null, group, event.dataContext, JBPopupFactory.ActionSelectionAid.NUMBERING, false, "what this")
+            .createActionGroupPopup(null, group, event.dataContext, JBPopupFactory.ActionSelectionAid.NUMBERING, false, null)
     }
 
     override fun update(e: AnActionEvent) {
@@ -31,6 +30,14 @@ class ProjectToolbarWidgetAction2 : ExpandableComboAction() {
         val projectName = project?.name ?: ""
         e.presentation.setText(projectName, false)
         e.presentation.description = projectName
+
+        val moduleName = project?.modules?.first()?.name ?: ""
+
+        if (projectName == moduleName || moduleName.isEmpty()) {
+            e.presentation.isVisible = false
+        } else {
+            e.presentation.setText(moduleName, false)
+        }
 
     }
 }
